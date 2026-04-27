b1cd7f301f7d79018f5e3d27166da6bb2f38efa1
fatima <fatima.velez@iteso.mx>
2026-04-27 10:44:00 -0600

    estructura general de backend

---
+++ BACK END/server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const router = require('./routes/api.js');
+const app=express();
+const port = 3000;
+
+
+app.use(express.json());
+app.use(express.static('../FRONTEND'));
+app.use('/controllers', express.static('../FRONTEND/controllers'));
+app.use('/views', express.static('../FRONTEND/views'));
+app.use('/assets', express.static('../FRONTEND/assets'));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Práctica 4 corriendo en el puerto ${port}!`);
+});
